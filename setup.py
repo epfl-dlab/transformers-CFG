e9716ac80c4517c4c09c2a22d6e033d7ef464835
Saibo-creator <53392976+Saibo-creator@users.noreply.github.com>
2024-02-13 16:31:17 +0100

    add __version__ in project (#7)

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+from transformers_cfg import __version__
 
 setup(
     name="transformers_cfg",
-    version="0.1.0",
+    version=__version__,
     author="EPFL-dlab",
     author_email="saibo.geng@epfl.ch",
-    description="Extension of Transformers library for Context-Free Grammar Constrained Decoding",
+    description="Extension of Transformers library for Context-Free Grammar Constrained Decoding with EBNF grammars",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/epfl-dlab/transformers-CFG",
--- transformers_cfg/__init__.py
+++ transformers_cfg/__init__.py
@@ -1,3 +1,5 @@
 from .logging_config import setup_logging
 
 setup_logging()
+
+__version__ = "0.1.1"
