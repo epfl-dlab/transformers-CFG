16f3932fca3aaf1643fceb928fde17f0f16045db
Arina Rak <arina.ruck@gmail.com>
2024-03-19 16:18:14 +0100

    Output verification added

--- examples/generate_geo_query.py
+++ examples/generate_geo_query.py
@@ -8,7 +8,7 @@ from transformers_cfg.parser import parse_ebnf
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Generate geo query strings")
-    parser.add_argument("--model_id", type=str, default="/dlabdata1/llm_hub/Mistral-7B-v0.1", help="Model ID")
+    parser.add_argument("--model-id", type=str, default="/dlabdata1/llm_hub/Mistral-7B-v0.1", help="Model ID")
     parser.add_argument("--device", type=str, help="Device to put the model on")
     return parser.parse_args()
 
@@ -36,7 +36,7 @@ def main():
     grammar_processor = GrammarConstrainedLogitsProcessor(grammar)
 
     # Generate
-    propmts = [
+    prompts = [
         "which state contains most rivers ? ",
         "number of citizens in boulder ? ",
         "what are the major cities of the us ? ",
@@ -45,7 +45,7 @@ def main():
     ]
 
     input_ids = tokenizer(
-        propmts, add_special_tokens=False, return_tensors="pt", padding=True
+        prompts, add_special_tokens=False, return_tensors="pt", padding=True
     )["input_ids"].to(
         device
     )  # Move input_ids to the same device as model
@@ -69,23 +69,30 @@ def main():
         num_return_sequences=1,
     )
 
-    # decode output
+    parsed_grammar = parse_ebnf(grammar_str)
+    string_grammar = StringRecognizer(parsed_grammar.grammar_encoding, parsed_grammar.symbol_table["root"])
+    
+    # decode outputs (possibly of different lengths across decoding modes)
+    generations = tokenizer.batch_decode(unconstrained_output, skip_special_tokens=True) + \
+                  tokenizer.batch_decode(constrained_output, skip_special_tokens=True)
     print()
-    generations = tokenizer.batch_decode(torch.concat([unconstrained_output, constrained_output]), skip_special_tokens=True)
     for i in range(n_examples):
         print(f"Unconstrained: {generations[i]}")
+        constrained_generation = generations[i + n_examples]
         print(f"Constrained: {generations[i + n_examples]}")
+        print(f'The constrained generation matches the grammar: {string_grammar._accept_string(constrained_generation[len(prompts[i]):])}')
+        print(f'The generated prefix matches the grammar: {string_grammar._accept_prefix(constrained_generation[len(prompts[i]):])}')
         print()
 
 if __name__ == "__main__":
     main()
 
 
-###############
+##########################
 # Example output: 
 #    
 # Unconstrained: how many states border colorado and border new mexico ? 1.
 # - How long is the drive from denver to albuquerque? The distance between Denver, Colorado (CO) & Alburqueque New Mexico(NM). Driving directions for your road trip or vacation: Get driving
 # Constrained: how many states border colorado and border new mexico ? answer(smallest_one(area_1(stateid('colorado'))))
 #
-###############
+##########################
