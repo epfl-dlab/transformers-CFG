9511ec9edfd51064b86767eeac9cc8d369613981
Saibo-creator <53392976+Saibo-creator@users.noreply.github.com>
2024-04-13 21:35:08 +0200

    add accept.py in example, which are used to check if a sentence is accepted by a grammar; fix `geo_query.ebnf` path in test_geo_query.py (#33)

---
+++ examples/accept.py
@@ -0,0 +1,54 @@
+import argparse
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers_cfg.grammar_utils import IncrementalGrammarConstraint
+from transformers_cfg.generation.logits_process import GrammarConstrainedLogitsProcessor
+import logging
+
+from transformers_cfg.parser import parse_ebnf
+from transformers_cfg.recognizer import StringRecognizer
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main(args):
+
+    with open(args.grammar_file_path, "r") as file:
+        grammar_str = file.read()
+    parsed_grammar = parse_ebnf(grammar_str)
+    start_rule_id = parsed_grammar.symbol_table["root"]
+    recognizer = StringRecognizer(parsed_grammar.grammar_encoding, start_rule_id)
+
+    if args.mode == "prefix":
+        result = recognizer._accept_prefix(args.sentence)
+    else:
+        result = recognizer._accept_string(args.sentence)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate text with grammar constraints."
+    )
+    parser.add_argument(
+        "-g",
+        "--grammar_file_path",
+        type=str,
+        required=True,
+        help="Path to the grammar file (supports both relative and absolute paths)",
+    )
+    parser.add_argument(
+        "-s", "--sentence", type=str, required=True, help="Prefix prompt for generation"
+    )
+    parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        choices=["prefix", "sentence"],
+        default="prefix",
+        help="Mode of operation, "
+        "prefix mode accepts a prefix string, sentence mode only accepts a full sentence",
+    )
+
+    args = parser.parse_args()
+    main(args)
