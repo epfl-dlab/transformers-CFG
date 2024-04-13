18ec0198754cca4a060aff663006da148cccd13a
Saibo Geng <saibo.geng@yahoo.fr>
2024-04-13 21:32:04 +0200

    add accept.py in example, which are used to check if a sentence is accepted by a grammar; fix `geo_query.ebnf` path in test_geo_query.py

--- tests/test_string_recognizer/test_geo_query.py
+++ tests/test_string_recognizer/test_geo_query.py
@@ -1,10 +1,10 @@
-
 from unittest import TestCase
 
 import logging
 from transformers_cfg.parser import parse_ebnf
 from transformers_cfg.recognizer import StringRecognizer
 from dataclasses import dataclass
+
 
 @dataclass
 class GeoQueryTestCase:
@@ -18,10 +18,18 @@ valid_geo_query_sentences = [
     GeoQueryTestCase("river", "answer(river(all))"),
     GeoQueryTestCase("state", "answer(loc_1(major(river(all))))"),
     GeoQueryTestCase("next_to_2", "answer(state(next_to_2(stateid('texas'))))"),
-    GeoQueryTestCase("intersection", "answer(intersection(state(next_to_2(stateid('texas'))), loc_1(major(river(all)))))"),
+    GeoQueryTestCase(
+        "intersection",
+        "answer(intersection(state(next_to_2(stateid('texas'))), loc_1(major(river(all)))))",
+    ),
     GeoQueryTestCase("space in name", "answer(population_1(stateid('new york')))"),
-    GeoQueryTestCase("exclude", "answer(count(exclude(river(all), traverse_2(state(loc_1(capital(cityid('albany', _))))))))"),
-    GeoQueryTestCase("city_id_with_state", "answer(population_1(cityid('washington', 'dc')))")
+    GeoQueryTestCase(
+        "exclude",
+        "answer(count(exclude(river(all), traverse_2(state(loc_1(capital(cityid('albany', _))))))))",
+    ),
+    GeoQueryTestCase(
+        "city_id_with_state", "answer(population_1(cityid('washington', 'dc')))"
+    ),
 ]
 
 valid_geo_query_prefixes = [
@@ -35,19 +43,22 @@ invalid_geo_query_sentences = [
     GeoQueryTestCase("unexisting_function", "answer(population_2(stateid('hawaii')))"),
     GeoQueryTestCase("empty_operator", "answer(highest(place(loc_2())))"),
     GeoQueryTestCase("empty_paranthesis", "()"),
-    GeoQueryTestCase("missing_argument", "answer(intersection(state(next_to_2(stateid('texas'))), )"),
+    GeoQueryTestCase(
+        "missing_argument", "answer(intersection(state(next_to_2(stateid('texas'))), )"
+    ),
 ]
-
 
 
 class Test_parsing_geo_query_object(TestCase):
     def setUp(self):
-        with open(f"examples/grammars/SMILES/geo_query.ebnf", "r") as file:
+        with open(f"examples/grammars/geo_query.ebnf", "r") as file:
             input_text = file.read()
         parsed_grammar = parse_ebnf(input_text)
         start_rule_id = parsed_grammar.symbol_table["root"]
-        self.recognizer = StringRecognizer(parsed_grammar.grammar_encoding, start_rule_id)
-        print('SetUp successfull!', flush=True)
+        self.recognizer = StringRecognizer(
+            parsed_grammar.grammar_encoding, start_rule_id
+        )
+        print("SetUp successfull!", flush=True)
 
     def test_valid_sentence(self):
 
@@ -57,7 +68,9 @@ class Test_parsing_geo_query_object(TestCase):
                 self.recognizer._accept_string(geo_query_test_case.geo_query),
                 msg=f"Failed on {geo_query_test_case.name}, {geo_query_test_case.geo_query}",
             )
-        for geo_query_test_case in valid_geo_query_prefixes + invalid_geo_query_sentences:
+        for geo_query_test_case in (
+            valid_geo_query_prefixes + invalid_geo_query_sentences
+        ):
             self.assertEqual(
                 False,
                 self.recognizer._accept_string(geo_query_test_case.geo_query),
@@ -70,11 +83,11 @@ class Test_parsing_geo_query_object(TestCase):
                 True,
                 self.recognizer._accept_prefix(geo_query_test_case.geo_query),
                 msg=f"Failed on {geo_query_test_case.name}, {geo_query_test_case.geo_query}",
-        )
+            )
 
         for geo_query_test_case in invalid_geo_query_sentences:
             self.assertEqual(
                 False,
                 self.recognizer._accept_prefix(geo_query_test_case.geo_query),
-                msg=f"Failed on {geo_query_test_case.name}, {geo_query_test_case.geo_query}"
+                msg=f"Failed on {geo_query_test_case.name}, {geo_query_test_case.geo_query}",
             )
