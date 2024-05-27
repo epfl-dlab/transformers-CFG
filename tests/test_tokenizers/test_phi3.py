19f9e4e38e825d98a734b8e82e41bbcf4f63ce21
Arina Rak <arina.ruck@gmail.com>
2024-05-27 16:02:45 +0200

    mapping refactored + sanity checks for t5/bloom/llama3 and t5 added

--- tests/test_tokenizers/test_phi3.py
+++ tests/test_tokenizers/test_phi3.py
@@ -7,10 +7,10 @@ from tests._tokenizer_common import TokenizerTesterMixin
 import logging
 
 
-class T5TokenizerTest(TokenizerTesterMixin, unittest.TestCase):
+class Phi3TokenizerTest(TokenizerTesterMixin, unittest.TestCase):
 
     tokenizer_class = T5TokenizerFast
-    pretrained_name = "t5-small"
+    pretrained_name = "microsoft/Phi-3-mini-4k-instruct"
 
     def setUp(self):
         super().setUp()
--- transformers_cfg/tokenization/mapping.py
+++ transformers_cfg/tokenization/mapping.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from transformers_cfg.utils import get_tokenizer_model_type, ints2bytes
 from transformers import AutoTokenizer
+import re
 import logging
 
 log = logging.getLogger(__name__)
@@ -10,30 +11,37 @@ log = logging.getLogger(__name__)
 def get_mapping(tokenizer, unicode=False):
     log.debug(f"tokenizer type: {tokenizer.__class__.__name__}")
     log.debug(f"tokenizer model type: {get_tokenizer_model_type(tokenizer)}")
+    tokenizer_name = tokenizer.__class__.__name__.lower()
     if not unicode:
-        if (
-            "gpt2" in tokenizer.__class__.__name__.lower()
-            or "bloom" in tokenizer.__class__.__name__.lower()
-            or "pretrainedtokenizer" in tokenizer.__class__.__name__.lower()
-            or "codegen" in tokenizer.__class__.__name__.lower()
-            or "gptneox" in tokenizer.__class__.__name__.lower()
+        if re.match(
+            r"gpt2|bloom|pretrainedtokenizer|codegen|gptneox|Llama-3", tokenizer_name
         ):
             return BBPEMapping(tokenizer)
-        elif "t5" in tokenizer.__class__.__name__.lower():
+        elif re.match(r"t5|Phi-3", tokenizer_name):
             return BPEMapping(tokenizer)
-        elif "llama" in tokenizer.__class__.__name__.lower():
+        elif "llama" in tokenizer_name:
             return LlamaBPEMapping(tokenizer)
-        elif "xglm" in tokenizer.__class__.__name__.lower():
+        elif "xglm" in tokenizer_name:
             return UniGramMapping(tokenizer)
         else:
             raise ValueError(f"Unknown tokenizer type: {tokenizer.__class__.__name__}")
     else:
-        if "gpt2" in tokenizer.__class__.__name__.lower():
+        if "gpt2" in tokenizer_name:
             return UnicodeBBPEMapping(tokenizer)
         else:
             raise NotImplementedError(
                 f"Unicode mapping for {tokenizer.__class__.__name__}"
             )
+
+
+class ReplacePrefixMixin:
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+    def _replace_prefix(self, token: str) -> str:
+        if token.startswith(self.prefix):
+            return token.replace(self.prefix, "", 1)
+        return token
 
 
 class Mapping:
@@ -48,20 +56,23 @@ class Mapping:
         return self._length
 
     def _map(self, token_id: int) -> str:
-        # This is the case for BOS,
-        if token_id in self.special:
-            return ""
         # if token_id is tensor, convert it to int
         if hasattr(token_id, "item"):
             token_id = token_id.item()
+        # This is the case for BOS,
+        if token_id in self.special:
+            return ""
         raw_token = self.tokenizer.convert_ids_to_tokens(token_id)
         return raw_token
+
+    def _encode(self, token: str) -> bytes:
+        return bytes(token, "utf-8")
 
     def map(self, token_id: int, verbose=False) -> bytes:
         token = self._map(token_id)
         if verbose:
             log.debug(f"token_id: {token_id}, token: {token}")
-        return bytes(token, "utf-8")
+        return self._encode(token)
 
 
 class BBPEMapping(Mapping):
@@ -71,7 +82,7 @@ class BBPEMapping(Mapping):
     def _map(self, token_id: int) -> str:
         raw_token = super()._map(token_id)
         if raw_token.startswith("Ġ"):
-            raw_token = raw_token.replace("Ġ", " ")
+            raw_token = raw_token.replace("Ġ", " ", 1)
         return raw_token
 
 
@@ -82,17 +93,8 @@ class UnicodeBBPEMapping(Mapping):
             self.tokenizer
         )
 
-    def _map(self, token_id: int, verbose=False) -> str:
-        raw_token = super()._map(token_id)
-        # if raw_token.startswith("Ġ"):
-        #     raw_token = raw_token.replace("Ġ", " ")
-        return raw_token
-
-    def map(self, token_id: int, verbose=False) -> bytes:
-        raw_token = self._map(token_id, verbose)
-        if verbose:
-            log.debug(f"token_id: {token_id}, raw_token: {raw_token}")
-        return self.intermediate_encoding.token2bytes(raw_token)
+    def _encode(self, token: str) -> bytes:
+        return self.intermediate_encoding.token2bytes(token)
 
     @staticmethod
     def get_intermediate_encoding(tokenizer):
@@ -107,17 +109,19 @@ class BPEMapping(Mapping):
         super().__init__(tokenizer)
         self.last_token_id = None
 
+    def _check_bos_token(self, token_id: int) -> bool:
+        # specific to BPE
+        at_bos = self.last_token_id is None
+        self.last_token_id = token_id if token_id != self.eos_token_id else None
+        return at_bos
+
     def _map(self, token_id: int) -> str:
         raw_token = super()._map(token_id)
-
         # we need to check if the token is at the beginning of the sentence to remove the space
         # specific to BPE
-        at_bos = False
-        if self.last_token_id is not None and self.last_token_id == self.bos_token_id:
-            at_bos = True
-        self.last_token_id = token_id
+        at_bos = self._check_bos_token(token_id)
         if raw_token.startswith("▁"):
-            raw_token = raw_token.replace("▁", " ")
+            raw_token = raw_token.replace("▁", " ", 1)
             if at_bos:
                 # remove space at the beginning of the sentence
                 raw_token = raw_token[1:]
@@ -127,6 +131,11 @@ class BPEMapping(Mapping):
 class LlamaBPEMapping(BPEMapping):
     def __init__(self, tokenizer):
         super().__init__(tokenizer)
+
+    def _check_bos_token(self, token_id: int) -> bool:
+        at_bos = self.last_token_id and (self.last_token_id == self.bos_token_id)
+        self.last_token_id = token_id
+        return at_bos
 
     def _map(self, token_id: int) -> str:
         raw_token = super()._map(token_id)
@@ -183,11 +192,11 @@ class ByteEncoding:
         self.char2byte: Dict[str, int] = tokenizer.byte_decoder
         # code point to byte
         self.cdp2byte: Dict[int, int] = {ord(c): b for c, b in self.char2byte.items()}
-        self.byte2cdp: Dict[int, int] = {v: k for k, v in self.cdp2byte.items()}
+        self.byte2cdp: Dict[int, int] = {b: c for c, b in self.cdp2byte.items()}
 
     def map(self, byte: int) -> int:
         assert 0 <= byte < 256, f"byte: {byte} is not in the range [0, 256)"
-        return ord(self.byte2char[byte])
+        return self.byte2cdp[byte]
 
     def token_ids2bytes(self, token_ids: List[int]) -> bytes:
         tokens: List[str] = self.tokenizer.convert_ids_to_tokens(token_ids)
@@ -196,10 +205,14 @@ class ByteEncoding:
         tokens = [
             "" if token in self.tokenizer.all_special_ids else token for token in tokens
         ]
-        bytes: List[List[int]] = [self.token2bytes(token) for token in tokens]
+        bytes_per_token: List[List[int]] = [self.token2bytes(token) for token in tokens]
         # join the bytes
-        return ints2bytes(sum(bytes, []))
+        bytes = sum(bytes_per_token, [])
+        # verify range and convert to bytes
+        bytes = ints2bytes(bytes)
+        return bytes
 
+    # Not used
     def token_id2bytes(self, token_id: int) -> bytes:
         token: str = self.tokenizer.convert_ids_to_tokens(token_id)
         return self.token2bytes(token)
