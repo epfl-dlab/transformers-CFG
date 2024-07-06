448842122b022c1242e9af09fa504a6cd756b9a1
Saibo Desktop <saibo.geng@yahoo.fr>
2024-07-06 06:49:44 +0200

    introduce Tokenizer class, which provides a common interface to support t-cfg, fix white space issue with Bart models

--- transformers_cfg/tokenization/vocab_struct.py
+++ transformers_cfg/tokenization/vocab_struct.py
@@ -4,6 +4,9 @@
 
 import logging
 import re
+from typing import List
+
+from transformers_cfg.tokenization.tokenizer import Tokenizer
 
 logger = logging.getLogger(__name__)
 
@@ -11,68 +14,23 @@ LEAF = -1
 
 
 class TokenTrie:
-    def __init__(self, tokenizer):
-        self.eos_token_id = tokenizer.eos_token_id
-        self.tokens = []
+    # TODO, should be phased out by only using ByteTrie
+    def __init__(self, hf_tokenizer):
+        self.eos_token_id = hf_tokenizer.eos_token_id
+        self.tcfg_tokenizer = Tokenizer.from_hf_tokenizer(hf_tokenizer)
         self.trie = {}
-        self.load_tokens(tokenizer)
+
+        self.load_token_as_bytes()
 
     def id2str(self, token_id):
         return self.tokens[token_id]
 
     def __len__(self):
-        return len(self.tokens)
+        return self.tcfg_tokenizer.real_vocab_size()
 
-    def load_tokens(self, tokenizer):
-        def replace_hex(match):
-            hex_value = match.group(1)
-            return chr(int(hex_value, 16))
-
-        if (
-            "gpt2" in tokenizer.__class__.__name__.lower()
-            or "pretrained" in tokenizer.__class__.__name__.lower()
-        ):  # llama3 tokenizer
-            special = tokenizer.additional_special_tokens_ids
-
-            # Here, the decoder does a string replace on a bunch of sequences
-            # like ' .' for '.'. This interferes with our assumptions, where a
-            # token should always have exactly one representation.
-            # Fortunately(?) text-generation-inference doesn't seem to run this
-            # cleanup, so we get extraneous spaces. So, in order to generate
-            # the right token set for TGI, we have to skip the space trimming.
-            # See:
-            # https://github.com/huggingface/transformers/blob/main/src/transformers/tokenization_utils_base.py#L3588-L3600
-            def fmt_token(id):
-                if id in special:
-                    return None
-                return bytes(
-                    tokenizer.decode([id], clean_up_tokenization_spaces=False), "utf-8"
-                )
-
-        elif (
-            "llama" in tokenizer.__class__.__name__.lower()
-            or "t5" in tokenizer.__class__.__name__.lower()
-        ):
-
-            def fmt_token(id):
-                token = tokenizer.convert_ids_to_tokens(id)
-                token = re.sub(r"<0x([0-9a-fA-F]{2})>", replace_hex, token)
-                token = token.replace("▁", " ")
-                return bytes(token, "utf-8")
-
-        else:
-            logger.warning(
-                "Warning: unrecognized tokenizer: using default token formatting"
-            )
-
-            def fmt_token(id):
-                token = tokenizer.convert_ids_to_tokens(id)
-                return bytes(token, "utf-8")
-
-        # note: vocab_size doesn't work here because there are also
-        # get_added_vocab() tokens
-        self.tokens = [fmt_token(i) for i in range(len(tokenizer.get_vocab()))]
-        for token_id, token_bytes in enumerate(self.tokens):
+    def load_token_as_bytes(self):
+        tokens: List[bytes] = self.tcfg_tokenizer.get_tokens_as_bytes()
+        for token_id, token_bytes in enumerate(tokens):
             if token_bytes is not None:
                 self.insert_into_trie(self.trie, token_bytes, token_id)
 
