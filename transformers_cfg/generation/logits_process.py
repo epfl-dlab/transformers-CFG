f2894f571e3fe85a0128246fdc80524eebf5c52e
Saibo-creator <53392976+Saibo-creator@users.noreply.github.com>
2024-08-22 16:38:19 +0800

    Remove profiling decorator from GrammarConstrainedLogitsProcessor (#82)
    Co-authored-by: Saibo MBP <guncyber@yahoo.com>

--- transformers_cfg/generation/logits_process.py
+++ transformers_cfg/generation/logits_process.py
@@ -5,7 +5,6 @@ import pprint
 
 import torch
 import logging
-from line_profiler import profile
 from transformers.generation.logits_process import (
     LogitsProcessor,
     LOGITS_PROCESSOR_INPUTS_DOCSTRING,
@@ -101,7 +100,6 @@ class GrammarConstrainedLogitsProcessor(LogitsProcessor):
         return masked_scores
 
     @add_start_docstrings(LOGITS_PROCESSOR_INPUTS_DOCSTRING)
-    @profile
     def __call__(
         self, input_ids: torch.LongTensor, scores: torch.FloatTensor
     ) -> torch.FloatTensor:
