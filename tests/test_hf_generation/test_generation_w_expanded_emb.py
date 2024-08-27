cd1257be38133897c2928f9e72b310bbf8cbf370
Saibo Desktop <saibo.geng@yahoo.fr>
2024-08-27 04:41:55 +0200

    test: Added new test file  to validate case where model'embedding has mismatch with tokenizer's vocab

--- tests/test_hf_generation/test_generation_w_expanded_emb.py
+++ tests/test_hf_generation/test_generation_w_expanded_emb.py
@@ -4,14 +4,6 @@ from transformers_cfg.token_grammar_recognizer import IncrementalTokenRecognizer
 from transformers_cfg.generation.logits_process import GrammarConstrainedLogitsProcessor
 
 MODEL_IDS = [
-    "hf-internal-testing/tiny-random-GPTJForCausalLM",
-    "JackFram/llama-68m",
-    "hf-internal-testing/tiny-random-PhiForCausalLM",
-    "hf-internal-testing/tiny-random-gpt2",
-    # "hf-internal-testing/tiny-random-BlenderbotForCausalLM",
-]
-
-UNICODE_MODEL_IDS = [
     "JackFram/llama-68m",
 ]
 
@@ -37,6 +29,10 @@ class TestGreedyDecoding(TestCase):
             cls.models[model_id] = AutoModelForCausalLM.from_pretrained(model_id)
             cls.tokenizers[model_id] = AutoTokenizer.from_pretrained(model_id)
             cls.tokenizers[model_id].pad_token = cls.tokenizers[model_id].eos_token
+            # we expand the embedding layer to simulate the case where the model has a larger embedding layer than the tokenizer
+            cls.models[model_id].resize_token_embeddings(
+                10 + len(cls.tokenizers[model_id])
+            )
 
     def test_generate_only_number(self):
         # test greedy decoding with grammar constraints
@@ -122,86 +118,4 @@ class TestGreedyDecoding(TestCase):
             self.assertTrue(
                 check_parentheses(generation),
                 f"generations: {generation} is not a balanced parenthesis",
-            )
-
-    def test_generate_constant(self):
-        # test greedy decoding with grammar constraints
-        grammar_str = """
-        root ::= "abcdefghijklmnopqrstuvwxyz"
-        """
-
-        for model_id in MODEL_IDS:
-            model = self.models[model_id]
-            tokenizer = self.tokenizers[model_id]
-
-            grammar = IncrementalTokenRecognizer(
-                grammar_str, start_rule_name="root", tokenizer=tokenizer
-            )
-            grammar_processor = GrammarConstrainedLogitsProcessor(grammar)
-
-            prefix = "Generate a constant:"
-
-            input_ids = tokenizer(
-                [prefix], add_special_tokens=False, return_tensors="pt", padding=True
-            )["input_ids"]
-
-            output = model.generate(
-                input_ids,
-                do_sample=False,
-                pad_token_id=tokenizer.eos_token_id,
-                eos_token_id=tokenizer.eos_token_id,
-                num_beams=1,
-                max_new_tokens=40,
-                top_p=0.92,
-                top_k=5,
-                logits_processor=[grammar_processor],
-                repetition_penalty=100.0,
-                early_stopping=True,
-            )
-
-            generations = tokenizer.batch_decode(
-                output[:, input_ids.shape[1] :], skip_special_tokens=True
-            )
-            self.assertTrue(
-                generations[0] == "abcdefghijklmnopqrstuvwxyz",
-                f"generations: {generations} is not equal to 'abcdefghijklmnopqrstuvwxyz'",
-            )
-
-    def test_generate_emoji(self):
-        # test greedy decoding with grammar constraints
-        grammar_str = """
-        root ::= "🤣"
-        """
-
-        for model_id in UNICODE_MODEL_IDS:
-            model = self.models[model_id]
-            tokenizer = self.tokenizers[model_id]
-
-            grammar = IncrementalTokenRecognizer(
-                grammar_str, start_rule_name="root", tokenizer=tokenizer, unicode=True
-            )
-            grammar_processor = GrammarConstrainedLogitsProcessor(grammar)
-
-            prefix = "Generate an emoji:"
-
-            input_ids = tokenizer(
-                [prefix], add_special_tokens=False, return_tensors="pt", padding=True
-            )["input_ids"]
-
-            output = model.generate(
-                input_ids,
-                do_sample=False,
-                pad_token_id=tokenizer.eos_token_id,
-                eos_token_id=tokenizer.eos_token_id,
-                num_beams=1,
-                max_new_tokens=40,
-                logits_processor=[grammar_processor],
-                early_stopping=True,
-            )
-
-            generations = tokenizer.batch_decode(
-                output[:, input_ids.shape[1] :], skip_special_tokens=True
-            )
-            self.assertTrue(
-                generations[0] == "🤣", f"generations: {generations} is not equal to '🤣'"
             )
