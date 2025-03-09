a4ab66bb668f28f14baca4e10cf3b8028859716e
Saibo-creator <53392976+Saibo-creator@users.noreply.github.com>
2025-03-09 15:05:59 +0100

    Add licence in  setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/epfl-dlab/transformers-CFG",
     packages=find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
